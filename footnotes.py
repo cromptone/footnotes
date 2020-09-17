b17769d751320f7372e7d28640be7804d6f4f9b4
Alexander Crompton <alexcromp@gmail.com>
2020-09-17 12:52:04 +0200

    Allow CORS

--- footnotes.py
+++ footnotes.py
@@ -1,5 +1,8 @@
 from flask import Flask, request
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/parse/<encoded_string>', methods=['GET'])
 def hello_world(encoded_string):
