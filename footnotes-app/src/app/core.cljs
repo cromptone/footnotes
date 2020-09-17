e8902a8f2b89b4456543bbed2c13065f05a35b28
Alexander Crompton <alexcromp@gmail.com>
2020-09-17 11:53:35 +0200

    Render Reagent paragraph on front-end

--- footnotes-app/src/app/core.cljs
+++ footnotes-app/src/app/core.cljs
@@ -1,4 +1,11 @@
-(ns app.core)
+(ns app.core (:require [reagent.dom :as r]))
+
+(defn view []
+  [:p "Footnotes app"])
+
+(defn start []
+  (r/render [view]
+    (.getElementById js/document "app")))
 
 (defn init []
-  (prn "Footnotes"))
+  (start))
